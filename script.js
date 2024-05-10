c3a59089e7a58f2ddd28106f40cc97f5ffbd38e2
syedtalha88 <150226683+syedtalha88@users.noreply.github.com>
2024-05-10 23:32:45 +0530

    final push

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let navbar = document.getElementById("navbar");
 let sticky = navbar.offsetTop;
 let moonbtn= document.getElementById('moon');
 let sunbtn= document.getElementById('sun');
+let burger= document.getElementById('burg');
+let navilist= document.getElementById('navlist');
 
 function myFunction() {
     if (window.scrollY >= sticky) {
@@ -16,4 +18,7 @@ function themechange(){
   document.body.classList.toggle('dark-theme');
   sunbtn.classList.toggle('hide');
   moonbtn.classList.toggle('show');
+}
+function navopen(){
+  navilist.classList.toggle('navshow');
 }
